90c592059dcf779a0b51f82a08267a4a5b0712db
Richard M <70453767+MorandR@users.noreply.github.com>
2021-11-17 18:30:32 -0500

    StartUp
    Page start up. Added a temporary MainPage along with adding folders for projects and resume

---
+++ morandr/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MainPage from './frontend/MainPage';
+
+function App() {
+  return (
+    <div className="App">
+    <MainPage/>
+    
+    </div>
+  );
+}
+
+export default App;
